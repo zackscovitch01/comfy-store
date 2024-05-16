486918f4852e9f336f4cb79f838965964bd9c3c6
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-16 05:45:09 -0400

    setupCart()

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -24,6 +24,12 @@ const display = (products, element) => {
         </article>`;
     })
     .join("");
+  element.addEventListener("click", function (e) {
+    const parent = e.target.parentElement;
+    if (parent.classList.contains("product-cart-btn")) {
+      addToCart(parent.dataset.id);
+    }
+  });
 };
 
 export default display;
